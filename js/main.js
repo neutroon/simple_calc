5111aea2ac434f2255cfbf18d3d15df443c29515
neutroon <191900312@ecu.edu.eg>
2023-05-02 15:34:08 +0300

    #

--- js/main.js
+++ js/main.js
@@ -8,14 +8,18 @@ function clearFields() {
     firstNum.value = "";
     secondNum.value = "";
     resultFiled.value = "";
+    resultFiled.style.display = "none";
 }
 
 function add() {
     let result = Number(firstNum.value) + Number(secondNum.value);
+    resultFiled.style.display = "block";
     resultFiled.value = result;
+
 }
 
 function subtract() {
     let result = Number(firstNum.value) * Number(secondNum.value);
+    resultFiled.style.display = "block";
     resultFiled.value = result;
 }
